91d80e68021d6e22c20a01a068681e0594ddbcdf
Haswell <haswell@HaswelldeMacBook-Air.local>
2024-09-17 23:08:08 +0800

    changed are made for ao connection

--- src/game.js
+++ src/game.js
@@ -1,50 +1,70 @@
 import {
-    init,
-    initKeys,
-    initPointer,
-    load,
-    onKey,
-    GameLoop,
-    dataAssets 
-} from 'kontra';
-import Scene from './scene.js';
+  init,
+  initKeys,
+  initPointer,
+  load,
+  onKey,
+  GameLoop,
+  dataAssets,
+} from "kontra";
+import Scene from "./scene.js";
 
-import {backgroundSpriteTiles, spriteTiles} from './tilesets.js';
-import {assetsToLoad, directionMapper} from './constants.js';
+import { backgroundSpriteTiles, spriteTiles } from "./tilesets.js";
+import { assetsToLoad, directionMapper } from "./constants.js";
 
 class Game {
-    constructor() {
-        this.currentScene = null;
-    }
-    
-    initGame = async () => {
-        init();
-        initKeys();
-        initPointer();
-        
-        this.addEventListeners();
+  constructor() {
+    this.currentScene = null;
+  }
 
-        dataAssets[new URL('m13e', location.href).href] = spriteTiles;
-        dataAssets[new URL('m13e-bg', location.href).href] = backgroundSpriteTiles;
+  initGame = async () => {
+    init();
+    initKeys();
+    initPointer();
 
-        await load(...assetsToLoad);
+    this.addEventListeners();
 
-        this.currentScene = new Scene();
-    }
+    dataAssets[new URL("m13e", location.href).href] = spriteTiles;
+    dataAssets[new URL("m13e-bg", location.href).href] = backgroundSpriteTiles;
 
-    addEventListeners = () => {
-        onKey(directionMapper.map(([key]) => key), event => event.preventDefault());
-    }
+    await load(...assetsToLoad);
 
-    start = async () => {
-        await this.initGame();
+    this.currentScene = new Scene();
+  };
 
-        GameLoop({
-            update: this.currentScene.update,
-            render: this.currentScene.render
-        }).start();
-    }
+  addEventListeners = () => {
+    onKey(
+      directionMapper.map(([key]) => key),
+      (event) => event.preventDefault()
+    );
+  };
+
+  start = async () => {
+    await this.initGame();
+
+    GameLoop({
+      update: this.currentScene.update,
+      render: this.currentScene.render,
+    }).start();
+  };
 }
+async function connectWallet() {
+  try {
+    // connect to the ArConnect browser extension
+    await window.arweaveWallet.connect(
+      // request permissions
+      ["ACCESS_ADDRESS", "SIGN_TRANSACTION", "ACCESS_PUBLIC_KEY", "SIGNATURE"]
+    );
+  } catch (error) {
+    alert("You should connect to ArConnect browser extension.");
+    return false;
+  }
 
-const game = new Game();
-game.start();
+  return true;
+}
+connectWallet().then((ret) => {
+  if (ret) {
+    const game = new Game();
+    game.start();
+  }
+});
